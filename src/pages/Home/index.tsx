1bcc48597e8ec99ad075a9982d091c0fd1a61270
Newton Peixoto <newt.peixoto@gmail.com>
2020-06-04 15:19:36 -0300

    maps and items in point page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { View,ImageBackground, Image, StyleSheet, Text } from 'react-native'
 import {Feather as Icon} from '@expo/vector-icons'
 import {RectButton} from 'react-native-gesture-handler'
+import {useNavigation} from '@react-navigation/native'
 
 const Home = () => {
+
+  const navigation = useNavigation()
+
+  function handleNavigateToPoints()
+  {
+    navigation.navigate('Points')
+  }
+
   return (
     <ImageBackground  
       imageStyle={{width:274, height:368}}
@@ -15,7 +24,7 @@ const Home = () => {
        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
      </View>
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
           <View style={styles.buttonIcon}>
             <Icon name="arrow-right" color="#fff" size={24}></Icon>
           </View>
@@ -31,8 +40,7 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 32,
-    backgroundColor:'#f0f0f5'
+    padding: 32
   },
 
   main: {
