efaa8e476aa073c506609a9cd8de204502c2c737
Newton Peixoto <newt.peixoto@gmail.com>
2020-06-04 15:42:08 -0300

    final structure of detail

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
-import {StyleSheet, View} from 'react-native'
+import {RectButton} from 'react-native-gesture-handler'
+import {useNavigation} from '@react-navigation/native'
+import {Feather as Icon, FontAwesome} from '@expo/vector-icons'
+import {StyleSheet, View, TouchableOpacity,Text, Image} from 'react-native'
 
 const Detail = () => {
-  return <View></View>
+
+  const navigation = useNavigation();
+  function handleNavigateBack()
+  {
+    navigation.goBack();
+  }
+  return ( 
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+            <Icon name="arrow-left" size={20} color="#34cb79"/> 
+          </TouchableOpacity>
+          <Image style={styles.pointImage} source={{
+            uri:'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+          }} />
+
+          <Text style={styles.pointName}> Mercado </Text>
+          <Text style={styles.pointItems}> Mercado </Text>
+
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}> Mercado </Text>
+            <Text style={styles.addressContent}> Mercado </Text>
+          </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={ () => {} }>
+          <FontAwesome name="whatsapp" size={20} color="#fff"/>
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={ () => {} }>
+          <Icon name="mail" size={20} color="#fff"/>
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View> 
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
